e0ee44b8cf8b9e43882dfbd1fc12a3c8fbba480b
Rafael Pinheiro <rafaelpinheiro.htb@gmail.com>
2022-08-22 11:55:11 -0300

    Primeiro arquivo do repositóri

---
+++ boolean.js
@@ -0,0 +1,9 @@
+const primeiroNumero = 5;
+const seguindoNumero = 5;
+
+console.log(primeiroNumero === seguindoNumero);
+
+const texto1 = "Alura";
+const texto2 = "a";
+
+console.log(texto1 == texto2);
