7f790cd4061f2c5e979b02ff62fc623f34901050
RafaelPinheirohtb <rafaelpinheiro.htb@gmail.com>
2022-08-23 16:21:24 -0300

    Operador ternário.js

---
+++ ternario.js
@@ -0,0 +1,12 @@
+// funciona como if / else
+
+const idadeMinima = 18;
+const idadeCliente = 19;
+
+// if(idadeCliente >= idadeMinima) {
+//     console.log('Cerveja')
+// } else {
+//     console.log('Suco')
+// }
+                    //condição             //true   //false
+console.log(idadeCliente >= idadeMinima ? 'Cerveja':'Suco') // Mesma função do if/else
