08e22375d7e2e044b66a1c1ecec7dbda93eea0a3
RafaelPinheirohtb <rafaelpinheiro.htb@gmail.com>
2022-08-27 15:45:59 -0300

    Aulas Alura

---
+++ juntando-turmas.js
@@ -0,0 +1,8 @@
+const salaDePython = ['Melissa', 'Milena', 'Rodrigo']
+
+const salaDeJavaScript = ['Ju', 'Léo', 'Raquel']
+
+// .concat uni arrays
+const salasUnificadas = salaDePython.concat(salaDeJavaScript)
+
+console.log(salasUnificadas)
---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,11 @@
+// índice         0        1         2       3
+const alunos = ['João', 'Juliana', 'Caio', 'Ana']
+
+// índice               0  1 2 3
+const mediaDosAlunos = [10,7,9,6]
+
+// índice                     0          1
+let listaDeNotasEAlunos = [alunos, mediaDosAlunos]
+
+// Resposta: João, sua média é: 10
+console.log(`${listaDeNotasEAlunos[0][0]}, sua média é: ${listaDeNotasEAlunos[1][0]}`)
