0c048aaae7ed99f96c52919211f95286ad8d87df
RafaelPinheirohtb <rafaelpinheiro.htb@gmail.com>
2022-10-05 11:40:35 -0300

    atualização

--- atualizando-lista.js
+++
@@ -1,9 +0,0 @@
-const listaDeChamada = ['João', 'Ana', 'Caio', 'Lara', 'Marjore', 'Leo']
-
-// .splice apartir do índice 1, remove 2 elementos e adiciona 'Rodrigo'
-//listaDeChamada.splice(1,2,'Rodrigo')
-
-// No índice 2, remove 0 elementos e adiciona 'Rodrigo'
-listaDeChamada.splice(2,0,'Rodrigo')
-
-console.log(`Lista de chamada: ${listaDeChamada}`)
--- juntando-turmas.js
+++
@@ -1,8 +0,0 @@
-const salaDePython = ['Melissa', 'Milena', 'Rodrigo']
-
-const salaDeJavaScript = ['Ju', 'Léo', 'Raquel']
-
-// .concat uni arrays
-const salasUnificadas = salaDePython.concat(salaDeJavaScript)
-
-console.log(salasUnificadas)
