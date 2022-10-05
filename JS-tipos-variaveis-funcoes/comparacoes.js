08ed56b6708f5160f7244acef344d1d5ff05a3f8
RafaelPinheirohtb <rafaelpinheiro.htb@gmail.com>
2022-10-05 14:15:10 -0300

    typeof

--- JS-tipos-variaveis-funcoes/comparacoes.js
+++ JS-tipos-variaveis-funcoes/comparacoes.js
@@ -3,13 +3,13 @@
 const numero = 5;
 const texto = '5';
 
-//console.log(numero == texto)// implicita = true
-console.log(numero === texto) // explicita = false
+console.log(numero == texto)  // implicita == true
+console.log(numero === texto) // explicita === false
 
-//typeof, descobrir o tipo de dado na variável
+//typeof, descobrir o tipo de dado na variável. Retorna o tipo de dado que a variável está armazenando
 
-console.log(typeof numero)
-console.log(typeof texto)
+console.log(typeof numero) // Retorna number
+console.log(typeof texto)  // Retorna string
 
 // == só compara o valor
 // === compara o valor e o tipo de dado
