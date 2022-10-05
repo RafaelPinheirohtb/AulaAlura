478b0fb7598240e30da1d85d5439ebd8bd30667d
RafaelPinheirohtb <rafaelpinheiro.htb@gmail.com>
2022-10-05 11:29:28 -0300

    for

---
+++ JS-arrays/for.js
@@ -0,0 +1,11 @@
+// números de elementos .length
+//                1    2    3    4    5    6
+//indice          0    1    2    3    4    5
+const numeros = [100, 200, 300, 400, 500, 600]
+
+for (let i = 0; i < numeros.length; i++) {
+//i inicia no 0; enquanto i for < que .length (6); acrescenta +1 no i
+  
+console.log(i, numeros[i])
+// mostra tos os i do .length e os números dos respectivos i(índice)
+}
