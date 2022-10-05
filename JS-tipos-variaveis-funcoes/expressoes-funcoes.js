358c20ec259ec4dd9becb371f4af5425d0bf0a32
RafaelPinheirohtb <rafaelpinheiro.htb@gmail.com>
2022-10-05 15:32:48 -0300

    Expressões-funções

--- JS-tipos-variaveis-funcoes/expressoes-funcoes.js
+++ JS-tipos-variaveis-funcoes/expressoes-funcoes.js
@@ -2,12 +2,14 @@ function minhaFuncao(param) {
     //bloco de código
 }
 
-// minhaFuncao("param")
+// minhaFuncao("param") = chama a função com ou sem parametros inseridos nela.
 
 //Expressão de função
 
 //const soma = function(num1, num2) {return num1 + num2}
-//console.log(soma(1, 1))
+//console.log(soma(1, 1)) // Retorno = 2
+
+//__________________________________________________________________
 
 // Diferença principal???
 // Funçoes e var são listados no topo(puxa para o topo do arquivo)
@@ -18,5 +20,5 @@ function apresentar() {
     return 'Olá!';
 }
 
-console.log(soma(1, 1))
+console.log(soma(1, 1)) //Retorno ERRO: Não consegue acessar SOMA antes da função.
 const soma = function(num1, num2) {return num1 + num2}
